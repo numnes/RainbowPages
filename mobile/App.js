58f52c6fef787ff8bd859e3e8bc87dc298bf8310
lukcaas <lukcaasgiovanini@gmail.com>
2021-01-16 11:53:31 -0300

    Alteração na pag. Inicial do app

--- mobile/App.js
+++ mobile/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>RainbowPagesfasdasd</Text>
+      <Text>RainbowPages ✨</Text>
       <StatusBar style="auto" />
     </View>
   );
