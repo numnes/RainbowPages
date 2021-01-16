49a2660e1556df26f912ad9bb1fc48bc049e53d9
lukcaas <lukcaasgiovanini@gmail.com>
2021-01-16 19:45:30 -0300

    Alterações nas pages do app

---
+++ mobile/src/pages/PerfilEmpresa/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Image, View } from 'react-native';
+import styles from './styles';
+
+function PerfilEmpresa(){
+    return (
+        <View style={styles.container}>
+        </View>
+    );
+}
+
+export default PerfilEmpresa;
